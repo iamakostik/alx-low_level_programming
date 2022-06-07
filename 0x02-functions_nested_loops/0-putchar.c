66979316bb47025d1c7036614ce69d218be00657
iamakostik <iamakostik@gmail.com>
2022-06-07 14:31:28 -0700

    putchar

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,17 @@
+#include "main.h"
+/**
+ * main - check the code
+ *
+ * Return: Always 0
+ */
+int main(void)
+{
+char pc[9] = "_putchar";
+int i;
+for (i = 0; i < 8; i++)
+{
+_putchar(pc[i]);
+}
+_putchar('\n');
+return (0);
+}
