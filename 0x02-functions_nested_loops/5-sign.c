e69e05dc50b551e28a534f461c1141c7023e421a
iamakostik <iamakostik@gmail.com>
2022-06-09 03:31:38 -0700

    sign

--- 0x02-functions_nested_loops/5-sign.c
+++ 0x02-functions_nested_loops/5-sign.c
@@ -1,7 +1,7 @@
 #include "main.h"
 /**
  * print_sign - check the code
- * @n - check for n
+ * @n : check for n
  * Return: Always 0 or 1 or -1
  */
 int print_sign(int n)
@@ -9,13 +9,13 @@ int print_sign(int n)
 if (n > 0)
 {
 _putchar('+');
-return(1);
+return (1);
 }
 else if (n == 0)
 {
 _putchar('0');
-return(0); 
+return (0);
 }
 _putchar('-');
-return(-1);
+return (-1);
 }
