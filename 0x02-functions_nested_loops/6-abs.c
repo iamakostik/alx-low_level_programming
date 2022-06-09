2a792bcbb69ab6db6d4277ddd3a8e25f80f74efd
iamakostik <iamakostik@gmail.com>
2022-06-09 03:46:16 -0700

    abs value

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,21 @@
+#include "main.h"
+/**
+ * _abs - check the code
+ * @n : check n
+ * Return: Always 0
+ */
+int _abs(int n)
+{
+if (n > 0)
+{
+return (n);
+}
+else if (n < 0)
+{
+return (-n);
+}
+else
+{
+return (n);
+}
+}
