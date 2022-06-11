19bbc01b96d350aeb319fdf8003320c22b1144dd
iamakostik <iamakostik@gmail.com>
2022-06-10 20:58:11 -0700

    yes

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,18 @@
+#include "main.h"
+/**
+ * print_numbers - check the code
+ *
+ */
+void print_numbers(void)
+{
+void print_numbers(void)
+{
+char i = 0;
+while (i <= 9)
+{
+_putchar(i + '0');
+i++;
+}
+_putchar('\n');
+}
+}
